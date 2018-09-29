fbcc488bc9e7ebcc136129611e5dcfe6f66ac48f
rupansh <rupanshsekar@hotmail.com>
2018-09-29 04:30:36 +0000

    bump health HAL to 2.0
    Signed-off-by: rupansh <rupanshsekar@hotmail.com>

--- device.mk
+++ device.mk
@@ -159,9 +159,8 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
