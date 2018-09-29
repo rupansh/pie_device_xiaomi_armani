ab5b6c23c065e941ef88689ec39d8b8c6f428b4f
rupansh <rupanshsekar@hotmail.com>
2018-09-29 13:32:47 +0000

    Fix thermal HAL
    Signed-off-by: rupansh <rupanshsekar@hotmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -66,7 +66,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
