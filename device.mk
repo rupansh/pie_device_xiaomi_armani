b9e6bf9bc6d2721d8e4de4598459cb2c9a1591c5
rupansh <rupanshsekar@hotmail.com>
2018-09-29 09:48:47 +0000

    Remove gatekeeper

--- device.mk
+++ device.mk
@@ -121,11 +121,6 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
-# Gatekeeper HAL
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
-
 # GNSS HAL
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
