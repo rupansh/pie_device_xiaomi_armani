1e904511f04f88d35e22def2bce4e4b74c831dc6
rupansh <rupanshsekar@hotmail.com>
2018-09-29 16:56:00 +0000

    armani doesn't have qti gnss
    oof
    
    Signed-off-by: rupansh <rupanshsekar@hotmail.com>

--- device.mk
+++ device.mk
@@ -124,8 +124,7 @@ PRODUCT_PACKAGES += \
 # GNSS HAL
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti
+    android.hardware.gnss@1.0-service
 
 # GPS
 PRODUCT_PACKAGES += \
